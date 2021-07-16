42897d9a7d55afe26e5cbb18970b8366c8086feb
Marcus Miguel <marcusmigueell@gmail.com>
2021-07-16 16:35:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,53 +11,78 @@
 
 	<body>
 		<nav class="navbar navbar-light bg-light">
+
 			<div class="container">
+
 				<a class="navbar-brand" href="#">
 					<img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
 					App Lista Tarefas
 				</a>
+
 			</div>
+
 		</nav>
 
 		<div class="container app">
+
 			<div class="row">
+
 				<div class="col-md-3 menu">
+
 					<ul class="list-group">
 						<li class="list-group-item active"><a href="#">Tarefas pendentes</a></li>
 						<li class="list-group-item"><a href="nova_tarefa.php">Nova tarefa</a></li>
 						<li class="list-group-item"><a href="todas_tarefas.php">Todas tarefas</a></li>
 					</ul>
+
 				</div>
 
 				<div class="col-md-9">
+
 					<div class="container pagina">
+
 						<div class="row">
+
 							<div class="col">
+
 								<h4>Tarefas pendentes</h4>
 								<hr />
 
 								<div class="row mb-3 d-flex align-items-center tarefa">
+
 									<div class="col-sm-9">Lavar o carro</div>
+
 									<div class="col-sm-3 mt-2 d-flex justify-content-between">
 										<i class="fas fa-trash-alt fa-lg text-danger"></i>
 										<i class="fas fa-edit fa-lg text-info"></i>
 										<i class="fas fa-check-square fa-lg text-success"></i>
 									</div>
+
 								</div>
 
 								<div class="row mb-3 d-flex align-items-center tarefa">
+
 									<div class="col-sm-9">Passear com o cachorro</div>
 									<div class="col-sm-3 mt-2 d-flex justify-content-between">
 										<i class="fas fa-trash-alt fa-lg text-danger"></i>
 										<i class="fas fa-edit fa-lg text-info"></i>
 										<i class="fas fa-check-square fa-lg text-success"></i>
 									</div>
+									
 								</div>
+
 							</div>
+
 						</div>
+
 					</div>
+
 				</div>
+
 			</div>
+
 		</div>
+
 	</body>
+
 </html>
